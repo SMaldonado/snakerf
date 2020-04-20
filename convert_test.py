b02840bed4288981daf16a3c77bfdece834b062a
Sasha Maldonado <engineerajm@gmail.com>
2020-04-19 23:23:19 -0700

    Added a whole bunch of inductor parasitic handling

--- convert_test.py
+++ convert_test.py
@@ -1,3 +1,28 @@
-import snakerf as srf
+# this file's filename no longer makes sense
 
-print(srf.dBm2Vp(-35))
+import snakerf as srf
+import matplotlib.pyplot as plt
+import numpy as np
+
+fig = plt.figure(figsize=(10,5))
+
+fspace = 1e3
+fstart = 1e5
+fstop = 200e6
+w = srf.f2w(np.arange(fstart, fstop, fspace))
+
+L = 22e-6
+ESR = 3.7
+SRF = 10e6
+Qref = 15
+fQref = 0.796e6
+
+ZL = srf.L(L, w, ESR=ESR, Qref=Qref, fQref=fQref, srf=SRF)
+
+plt.loglog(srf.w2f(w)/1.0e6, srf.mag(ZL))
+plt.grid()
+plt.gca().twinx()
+plt.plot(srf.w2f(w)/1.0e6, srf.phase(ZL, deg = True), c = 'orange')
+# plt.loglog(srf.w2f(w)/1.0e6, Q, c = 'purple')
+
+plt.show()
--- snakerf.py
+++ snakerf.py
@@ -1,5 +1,5 @@
 import math
-from math import pi
+from math import pi, inf
 import numpy as np
 from numpy import log10, rad2deg, deg2rad, sqrt
 
@@ -9,20 +9,80 @@ def par(Z1, Z2):
 def ser(Z1, Z2):
     return Z1 + Z2
 
-def phase(x, unwrap = True):
-    if unwrap:
-        return np.unwrap(np.angle(x))
-    else:
-        return np.angle(x)
+def mag(x):
+    return np.abs(x)
 
+def phase(x, unwrap = True, deg = False):
+    if unwrap:
+        angle = np.unwrap(np.angle(x))
+    else:
+        angle = np.angle(x)
+
+    if deg:
+        return rad2deg(angle)
+    else:
+        return angle
 
 # passive component frequency responses
 
 def C(C, w, ESR = 0):
     return (1/(1j*w*C)) + ESR
 
-def L(L, w, ESR = 0):
-    return 1j*w*L + ESR
+def L(L, w, ESR = 0, Cpar = 0, Gpar = 0, Qref = inf, fQref = inf, srf = inf, Zsrf = inf):
+
+    # Inductor model assumes the following parasitics:
+    #     |----Gpar----|
+    #     |----Cpar----|
+    #  ---|---L--ESR---|---
+    #
+    # Cpar can be provided or calculated from the self-resonant frequency
+    # Gpar can be provided or calculated from a known Q at a known frequency
+    # (or a known maximum impedance at resonance)
+
+    if Cpar != 0 and Gpar !=0: # use explicitly specified parasitics if available
+        return par(1j*w*L + ESR, par(C(Cpar, w), R(1/Gpar, w)))
+    else:
+        Cpar_eff = Cpar
+        Gpar_eff = Gpar
+
+        if Qref != inf and fQref != inf: # calculate Gpar from measured Q
+            wQref = f2w(fQref)
+
+            if ESR == 0:
+                Lp = L
+                Rp = Qref * wQref * Lp
+                Gpar_eff = 1/Rp
+            else: # transform series RL to parallel RL
+                Qs = wQref*L/ESR
+                Lp = L * (1 + Qs**2.0) / (Qs**2.0)
+                ESRp = (1 + Qs**2.0)*ESR
+                Rp = Qref * wQref * Lp
+                Gpar_eff = (1/Rp) - (1/ESRp)
+
+        if srf != inf and (ESR != 0 or Gpar_eff != 0 or Zsrf != inf):
+            wsrf = f2w(srf)
+
+            if ESR == 0:
+                Lp = L
+                if Zsrf != inf and Gpar_eff == 0:
+                    Gpar_eff = 1/Zsrf
+
+            else: # transform series RL to parallel RL
+                Qs = wQref*L/ESR
+                Lp = L * (1 + Qs**2.0) / (Qs**2.0)
+                ESRp = (1 + Qs**2.0)*ESR
+                if Zsrf != inf and Gpar_eff == 0:
+                    Gpar_eff = (1/Zsrf) - (1/ESRp)
+
+            Cpar_eff = 1/(Lp * wsrf**2.0)
+
+        if Cpar_eff != 0 and Gpar_eff !=0:
+            return par(1j*w*L + ESR, par(C(Cpar_eff, w), R(1/Gpar_eff, w)))
+        if Cpar_eff != 0:
+            return par(1j*w*L + ESR, C(Cpar_eff, w))
+        if Gpar_eff != 0:
+            return par(1j*w*L + ESR, R(1/Gpar_eff, w))
+        return 1j*w*L + ESR
 
 def R(R, w):
     return R * np.ones(len(w))
@@ -42,6 +102,12 @@ def undBv(x): # dB gain to linear voltage gain
     return 10.0**(x/20.0)
 
 # Unit conversions
+
+def f2w(x): # Hz to Rad/s
+    return 2.0*pi*x
+
+def w2f(x): # Rad/s to Hz
+    return x/(2.0*pi)
 
 def W2dBm(W): # power [W] to power [dBm]
     return dB(W) + 30.0
