Fixed data --> symbol conversion on all existing modulation schemes, fixed incorrect calculation of PSK phase deltas

fsk_test.py
@@ -3,27 +3,25 @@ import matplotlib.pyplot as plt
 import numpy as np
 from math import inf, pi, log2
 
+data = '011110010001'
+f = 2500000
 
-t = np.linspace(0,0.0399,100000)
-f = 1000
-
-fs = srf.fft_fs(t)
-ws = srf.f2w(fs)
-
-f_bit = f/2.1
+f_bit = 10000
 T_bit = 1/f_bit
 h = 0.5
 f_dev = h/(2*T_bit)
 print(f_dev)
 
-data = [1,-1,-1,1,-1,1,1,-1,1,-1,1,-1,1,-1,1,1,-1,-1,-1,-1]
+t = np.linspace(0,len(data)*T_bit - T_bit/100,100000)
+fs = srf.fft_fs(t)
+ws = srf.f2w(fs)
 
 v1 = srf.V_fsk(t, f, f_bit, f_dev, data, -100)
 v2 = srf.Vt_noise(t)
 v3 = srf.power_combine([v1,v2], t, out_Pf = True)
 # print(srf.C(1e-9, ws))
 R1 = 1e3
-C1 = 10e-9
+C1 = 25e-12
 v4 = v3 * srf.Vdiv(srf.R(R1, ws), srf.C(C1, ws))
 
 # print(srf.w2f(1/(R1*C1)))

msk_test.py
@@ -3,8 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from math import inf, pi, log2
 
-data = [1,1,-1,-1,1,-1,-1,1,1,-1]
-
+data = '011110010001'
 f = 2500000
 
 f_bit = 10000
@@ -17,14 +16,11 @@ t = np.linspace(0,len(data)*T_bit - T_bit/100,100000)
 fs = srf.fft_fs(t)
 ws = srf.f2w(fs)
 
-# data = [1,-1,-1,1,-1,1,1,-1,1,-1,1,-1,1,-1,1,1,-1,-1,-1,-1]
-
-
 # v1, inverted, df, odd, even, plt_data = srf.V_msk(t, f, f_bit, data, -100)
 v1 = srf.V_msk(t, f, f_bit, data, -100)
 v2 = srf.Vt_noise(t)
 v3 = srf.power_combine([v1,v2], t, out_Pf = True)
-# print(srf.C(1e-9, ws))
+
 R1 = 1e3
 C1 = 10e-9
 v4 = v3 * srf.Vdiv(srf.R(R1, ws), srf.C(C1, ws))

psk_test.py
@@ -4,33 +4,33 @@ import numpy as np
 from math import inf, pi, log2
 
 
-t = np.linspace(0,0.039,100000)
-f = 1000
+data = '011110010001'
+n = 1
+f = 25000
 
+f_bit = 9001
+T_bit = 1/f_bit
+
+t = np.linspace(0,len(data)*T_bit/n - T_bit/100,100000)
 fs = srf.fft_fs(t)
 ws = srf.f2w(fs)
 
-# print(srf.gold_codes(3))
-
-#
-v1 = srf.V_psk(t, f, f/10, [1,-1,-1,1], -100)
+v1 = srf.V_psk(t, f, f_bit, data, -100, n = n)
 v2 = srf.Vt_noise(t)
 v3 = srf.power_combine([v1,v2], t, out_Pf = True)
-# print(srf.C(1e-9, ws))
+
 R1 = 1e3
-C1 = 10e-9
+C1 = 50e-12
 v4 = v3 * srf.Vdiv(srf.R(R1, ws), srf.C(C1, ws))
 
-# print(srf.w2f(1/(R1*C1)))
+print(srf.w2f(1/(R1*C1)))
 
-# plt.subplot(4,1,1)
-# srf.plot_power_spectrum(plt.gca(), t, v1, time = True)
-# plt.subplot(4,1,2)
-# srf.plot_power_spectrum(plt.gca(), t, v2, time = True)
 plt.subplot(2,1,1)
 srf.plot_power_spectrum(plt.gca(), fs, v3, time = False)
-srf.plot_power_spectrum(plt.gca(), fs, v4, time = False)
+srf.plot_power_spectrum(plt.gca(), t, v1, time = True)
 plt.subplot(2,1,2)
 plt.plot(t, srf.Pf2Vt(v3, len(t)))
-plt.plot(t, srf.Pf2Vt(v4, len(t)))
+plt.plot(t, v1)
+for i in range(len(data)):
+    plt.axvline(T_bit * i, ls = '--', c = 'black')
 plt.show()

snakerf.py
@@ -192,41 +192,41 @@ def Vt_noise(t_sample, T_noise = room_temp, Z0 = 50): # create sampled time-doma
     V_stddev_noise = sqrt(V2_noise_Hz * f_sample / 2)
     return np.random.normal(0, V_stddev_noise, len(t_sample))
 
-# TODO: update to accept data, convert to symbols
-def V_psk(t_sample, fc, f_sym, data, dBm, n = 1): # create (2**n)-PSK modulated signal, with carrier fc and symbol rate f_sym
-    # expected symbols format: [x1, x2, ... xm], -m/2 <= xi <= m/2, m != 0
+
+def V_psk(t_sample, fc, f_sym, data, dBm, n = 1): # create (2**n)-PSK modulated signal (circular constellation), with carrier fc and symbol rate f_sym
+    # expected data format: "0100100101..." (spaces permitted for readability, will be ignored)
+
+    syms = data2sym(data, n)
 
     m = 2**n
-    d_phi = pi/m # get phase step # TODO: fix for n > 1; currently wrong
+    d_phi = 2*pi/m # get phase step
     T_sym = 1/f_sym # get symbol time
 
-    phi = d_phi * np.array([data[int(t/T_sym)] for t in t_sample])
+    phi = d_phi * np.array([syms[int(t/T_sym)] - 0.5*np.sign(syms[int(t/T_sym)])for t in t_sample])
 
     return dBm2Vp(dBm) * np.sin((f2w(fc) * t_sample) + phi)
 
-# TODO: update to accept data, convert to symbols
 def V_fsk(t_sample, fc, f_sym, f_dev, data, dBm, n = 1): # create (2**n)-FSK modulated signal, with carrier fc, symbol rate f_sym, deviation f_dev
-    # expected symbols format: [x1, x2, ... xm], -m/2 <= xi <= m/2, m != 0
+    # expected data format: "0100100101..." (spaces permitted for readability, will be ignored)
 
-    # m = 2**n # number of different states per symbol
+    syms = data2sym(data, n)
     T_sym = 1/f_sym # get symbol time
 
-    f = fc + f_dev * np.array([data[int(t/T_sym)] for t in t_sample])
+    f = fc + f_dev * np.array([syms[int(t/T_sym)] for t in t_sample])
 
     return dBm2Vp(dBm) * np.sin((f2w(f) * t_sample))
 
-# TODO: update to accept data, convert to symbols
 def V_msk(t_sample, fc, f_sym, data, dBm): # create MSK modulated signal, n = 1, m = 2
-    # expected symbols format: [x1, x2, ... xm], -m/2 <= xi <= m/2, m != 0
+    # expected data format: "0100100101..." (spaces permitted for readability, will be ignored)
     # see: https://www.dsprelated.com/showarticle/1016.php
     # see https://www.slideshare.net/mahinthjoe/lecture-5-1580898
-
+    syms = data2sym(data)
     T_sym = 1/f_sym # get bit time
     h = 0.5
     f_dev = h/(2*T_sym)
 
-    odd_bits = [data[2*i] for i in range(len(data)//2)]
-    even_bits = [-1] + [data[2*i + 1] for i in range(len(data)//2)]
+    odd_bits = [syms[2*i] for i in range(len(syms)//2)]
+    even_bits = [-1] + [syms[2*i + 1] for i in range(len(syms)//2)]
 
     inverted = np.array([odd_bits[int(t/(2*T_sym))] for t in t_sample])
     delta = np.array([(abs(odd_bits[int(t/(2*T_sym))] + even_bits[int((t+T_sym)/(2*T_sym))]) - 1) for t in t_sample])