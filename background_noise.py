Fixed factor of 4, use V2 = 4kTR from now on

background_noise.py
@@ -15,21 +15,18 @@ f = 1234
 f_bit = 9001
 T_bit = 1/f_bit
 # t_max = len(data)*T_bit/n - T_bit/100
-fs = 10e3
+fs = 500e6
 ns = 100000
 t_max = ns/fs
 
 
 V1 = srf.Signal(ns, t_max)
-# # V1.update_Vt(amp*np.sin(2*np.pi*freq*time))
-# V1.add_noise(noise = 0.001 /(4 * srf.kB * V1.Z0))
-# srf.plot_power_spectrum(plt.gca(), V1.fs, V1.Pf)
-# mean_noise = np.mean(srf.mag(V1.Pf * V1.Z0 / V1.df))
-# print(mean_noise)
-# plt.axhline(srf.W2dBm(mean_noise * V1.df/V1.Z0), c = 'k', ls = '--')
 
+V2 = srf.Signal(ns, t_max)
+V2.add_noise()
 
-# srf.plot_power_spectrum(plt.gca(), V1.ts, np.random.normal(0, 1, len(V1.ts)), time = True)
+print(srf.NF2T_noise(3))
+srf.plot_power_spectrum(plt.gca(), V2.fs, V2.Pf)
 
 f_ref =  [0, 4, 5, 8.3, 12] # log frequency
 Fa_ref = [270, 150, 80, 0, 0] # Fa = 10*log10(T_noise/t0)
@@ -38,11 +35,14 @@ V1.update_Pf(srf.Vt_background_noise(V1.ts, V1.fs))
 srf.plot_power_spectrum(plt.gca(), V1.fs, V1.Pf)
 
 T_noise = srf.undB(np.interp(np.log10(np.maximum(V1.fs,np.ones(len(V1.fs)))), f_ref, Fa_ref)) * srf.t0 # weird thing with ones to avoid log(0)
-plt.plot(V1.fs, srf.W2dBm(srf.kB*T_noise*V1.df))
+plt.plot(V1.fs, srf.W2dBm(4*srf.kB*T_noise*V1.df))
 
 N = 100
 moving_avg = np.convolve(srf.mag(V1.Pf * V1.Z0 / V1.df), np.ones((N,))/N, mode='valid') * V1.df/V1.Z0
 plt.plot(V1.fs[:-N+1], srf.W2dBm(moving_avg))
+
+moving_avg = np.convolve(srf.mag(V2.Pf * V2.Z0 / V2.df), np.ones((N,))/N, mode='valid') * V2.df/V2.Z0
+plt.plot(V2.fs[:-N+1], srf.W2dBm(moving_avg))
 
 
 plt.show()

snakerf.py
@@ -230,7 +230,7 @@ def Vt_background_noise(ts, fs, Z0 = 50):
 
     Pf_noise_white = Vt2Pf(V_noise_white, len(ts), Z0 = Z0)
     V2_Hz_mean_noise_white = np.mean(mag(Pf_noise_white * Z0 / df))
-    H_norm = V2_noise_Hz / (4*V2_Hz_mean_noise_white)
+    H_norm = V2_noise_Hz / (V2_Hz_mean_noise_white)
     Pf_noise = Pf_noise_white * H_norm
 
     # Total integrated power agrees decently well between PSD and output power spectrum