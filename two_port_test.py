766da7e59bcd9d60ab24f49d0855e02fbb8dbe1c
Sasha Maldonado <engineerajm@gmail.com>
2020-06-06 02:01:59 -0700

    Confirmed 2-ports are currently broken

--- two_port_test.py
+++ two_port_test.py
@@ -3,19 +3,20 @@ import matplotlib.pyplot as plt
 import numpy as np
 from math import inf, pi, log2
 
-f = np.logspace(1,7,1000)
+f = np.logspace(1,8,1000)
 w = srf.f2w(f)
 
 Z1 = srf.L(1e-6, w)
-Z2 = srf.Zopen(w) # srf.C(1e-9, w) #
+Z2 = srf.C(1e-9, w) #srf.Zopen(w) #
 
 filt = srf.Two_Port.from_network(f, [Z1], [Z2])
 V2 = filt.V_out(50*np.ones(len(f)), 50*np.ones(len(f)))
 
 print(V2)
 
-plt.plot(filt.fs, V2.real)
-plt.plot(filt.fs, V2.imag)
+plt.semilogx(filt.fs, srf.mag(V2))
+plt.twinx()
+plt.semilogx(filt.fs, np.rad2deg(srf.phase(V2)), ls = '--')
 plt.show()
 
 # srf.plot_power_spectrum(plt.gca(), filt.fs, srf.mag(V2)**2 / 50)
