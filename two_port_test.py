Sadly suspect Zin calculation is wrong

two_port_test.py
@@ -7,12 +7,10 @@ f = np.logspace(1,8,1000)
 w = srf.f2w(f)
 
 Z1 = srf.L(1e-6, w)
-Z2 = srf.Zopen(w) # srf.C(1e-9, w) #
+Z2 = srf.C(1e-9, w) # srf.Zopen(w) # 
 
 filt = srf.Two_Port.from_network(f, [Z1], [Z2])
 V2 = filt.V_out(50*np.ones(len(f)), 50*np.ones(len(f)))
-
-print(V2)
 
 plt.semilogx(filt.fs, srf.dBv(srf.mag(V2)))
 plt.twinx()