2c562da205781412217381173f269f8742824cb1
Sasha Maldonado <engineerajm@gmail.com>
2020-07-08 23:53:48 -0700

    painfully slow signal path progress

--- signal_path_test.py
+++ signal_path_test.py
@@ -9,9 +9,10 @@ from math import inf, pi, log2
 v1 = srf.Signal(10000, 0.1)
 v1.make_tone(100, 0)
 
+stage1 = srf.Two_Port.from_network(v1.fs, [srf.L(1e-6, v1.fs)], [srf.C(1e-9, v1.fs)])
+
 plt.subplot(2,1,1)
 v1.plot_t(plt.gca())
-plt.plot(v1.ts, -srf.Vf2Vt(v1.Vf, v1.ns))
 
 plt.subplot(2,1,2)
 plt.plot(v1.fs, srf.mag(v1.Vf))
